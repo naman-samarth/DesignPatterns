20b4cf76d7906a31cd9f8c327278957c605e9e67
Naman M Samarth <namansamarth1@gmail.com>
2022-08-04 18:00:23 +0530

    Facade Pattern

---
+++ src/structural/facade/Driver.java
@@ -0,0 +1,11 @@
+package structural.facade;
+
+import java.io.File;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
---
+++ src/structural/facade/README.md
@@ -0,0 +1,7 @@
+# Facade
+
+Provides a simplified interface to a library, a framework, or any other complex set of classes.
+
+### Applicability
+- When you need to have a limited but straightforward interface to a complex subsystem.
+- When you want to structure a subsystem into layers.
